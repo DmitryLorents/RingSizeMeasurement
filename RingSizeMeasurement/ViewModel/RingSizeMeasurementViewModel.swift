e67d7d27136bffb4d63c89b2cd0241d518871ce6
Dmitry <zmey.task@gmail.com>
2024-08-13 14:54:06 +0300

    Add folders

---
+++ RingSizeMeasurement/ViewModel/RingSizeMeasurementViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RingSizeMeasurementViewModel.swift
+//  RingSizeMeasurement
+//
+//  Created by lorenc_D_K on 13.08.2024.
+//
+
+import Foundation
+
+final class RingSizeMeasurementViewModel: ObservableObject {
+    
+}
