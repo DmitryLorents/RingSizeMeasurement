60fbe6c57475a643bf78c52c426cc3d314d0c714
Dmitry <zmey.task@gmail.com>
2024-08-13 11:27:46 +0300

    Initial Commit

---
+++ RingSizeMeasurement/RingSizeMeasurementApp.swift
@@ -0,0 +1,17 @@
+//
+//  RingSizeMeasurementApp.swift
+//  RingSizeMeasurement
+//
+//  Created by lorenc_D_K on 13.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct RingSizeMeasurementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
