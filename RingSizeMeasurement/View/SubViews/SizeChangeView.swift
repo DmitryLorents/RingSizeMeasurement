f5f059c1c7eb64b979121ed12784b051e974e1ae
Dmitry <zmey.task@gmail.com>
2024-08-20 10:09:10 +0300

    Fix slider tint

--- RingSizeMeasurement/View/SubViews/SizeChangeView.swift
+++ RingSizeMeasurement/View/SubViews/SizeChangeView.swift
@@ -4,13 +4,11 @@
 //
 //  Created by lorenc_D_K on 15.08.2024.
 //
-import CoreHaptics
 import SwiftUI
 
 struct SizeChangeView: View {
     
     @Binding var size: Float
-    @State private var engine: CHHapticEngine?
     var sizeValues: [Float]
     let step: Float
     let increaseAction: () -> Void
@@ -37,7 +35,7 @@ struct SizeChangeView: View {
                     .overlay(Image(.minus))
             })
             Slider(value: $size, in: minSize...maxSize, step: step)
-//                .accentColor(.pinkApp)
+                .accentColor(.pinkApp)
             
             Button(action: {
                 increaseAction()
@@ -52,9 +50,9 @@ struct SizeChangeView: View {
         
     }
     
-    private func formatSize() -> String {
-        let divisionReminder = size.truncatingRemainder(dividingBy: 1) == 0 ? 0 : 1
-        return String(format: "%0.\(divisionReminder)f", size)
-    }
+//    private func formatSize() -> String {
+//        let divisionReminder = size.truncatingRemainder(dividingBy: 1) == 0 ? 0 : 1
+//        return String(format: "%0.\(divisionReminder)f", size)
+//    }
 }
 // Link to Slider implementation with array of values https://stackoverflow.com/questions/68598052/is-there-a-way-to-define-discrete-steps-for-a-slider
